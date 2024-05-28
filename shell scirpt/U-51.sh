cd92ae711c3a02fa42a67db8a3bdd6af7485bcdb
971023als <971023als@naver.com>
2024-05-29 08:19:40 +0900

    Update U-51.sh

--- shell scirpt/U-51.sh
+++ shell scirpt/U-51.sh
@@ -1,16 +1,42 @@
 #!/bin/bash
 
-# 결과를 저장할 JSON 파일 초기화
-results_file="results.json"
-echo '{
-    "분류": "계정관리",
-    "코드": "U-51",
-    "위험도": "하",
-    "진단 항목": "계정이 존재하지 않는 GID 금지",
-    "진단 결과": "양호",
-    "현황": [],
-    "대응방안": "계정이 없는 불필요한 그룹 삭제"
-}' > $results_file
+. function.sh
+
+OUTPUT_CSV="output.csv"
+
+# Set CSV Headers if the file does not exist
+if [ ! -f $OUTPUT_CSV ]; then
+    echo "category,code,riskLevel,diagnosisItem,service,diagnosisResult,status" > $OUTPUT_CSV
+fi
+
+# Initial Values
+category="계정관리"
+code="U-51"
+riskLevel="하"
+diagnosisItem="계정이 존재하지 않는 GID 금지"
+service="Account Management"
+diagnosisResult=""
+status=""
+
+BAR
+
+CODE="U-51"
+diagnosisItem="계정이 존재하지 않는 GID 금지 검사"
+
+# Write initial values to CSV
+echo "$category,$CODE,$riskLevel,$diagnosisItem,$service,$diagnosisResult,$status" >> $OUTPUT_CSV
+
+TMP1=$(basename "$0").log
+> $TMP1
+
+BAR
+
+cat << EOF >> $TMP1
+[양호]: 계정이 없는 불필요한 그룹이 없는 경우
+[취약]: 계정이 없는 불필요한 그룹이 존재하는 경우
+EOF
+
+BAR
 
 if [ -f "/etc/group" ] && [ -f "/etc/passwd" ]; then
     # Extract GIDs in use from /etc/passwd
@@ -25,11 +51,24 @@ if [ -f "/etc/group" ] && [ -f "/etc/passwd" ]; then
     done < "/etc/group"
 
     if [ ${#unnecessary_groups[@]} -gt 0 ]; then
-        jq --arg groups "$(IFS=, ; echo "${unnecessary_groups[*]}")" '.진단 결과 = "취약" | .현황 += ["계정이 없는 불필요한 그룹이 존재합니다: " + $groups]' $results_file > tmp.$$.json && mv tmp.$$.json $results_file
+        diagnosisResult="계정이 없는 불필요한 그룹이 존재합니다: ${unnecessary_groups[*]}"
+        status="취약"
+        echo "WARN: $diagnosisResult" >> $TMP1
+    else
+        diagnosisResult="계정이 없는 불필요한 그룹이 존재하지 않습니다."
+        status="양호"
+        echo "OK: $diagnosisResult" >> $TMP1
     fi
 else
-    jq '.진단 결과 = "취약" | .현황 += ["/etc/group 또는 /etc/passwd 파일이 없습니다."]' $results_file > tmp.$$.json && mv tmp.$$.json $results_file
+    diagnosisResult="/etc/group 또는 /etc/passwd 파일이 없습니다."
+    status="취약"
+    echo "WARN: $diagnosisResult" >> $TMP1
 fi
 
-# 결과 출력
-cat $results_file
+echo "$category,$CODE,$riskLevel,$diagnosisItem,$service,$diagnosisResult,$status" >> $OUTPUT_CSV
+
+cat $TMP1
+
+echo ; echo
+
+cat $OUTPUT_CSV
